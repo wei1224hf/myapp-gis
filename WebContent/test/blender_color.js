62b2ca88217f7d7e4e0b18370b966c1a49741691
wei1224hf <wei1224hf@gmail.com>
2013-11-08 21:27:35 +0800

    floor room

---
+++ WebContent/test/blender_color.js
@@ -0,0 +1,74 @@
+{
+
+	"metadata" :
+	{
+		"formatVersion" : 3.1,
+		"generatedBy"   : "Blender 2.64 Exporter",
+		"vertices"      : 8,
+		"faces"         : 2,
+		"normals"       : 1,
+		"colors"        : 0,
+		"uvs"           : [],
+		"materials"     : 1,
+		"morphTargets"  : 0,
+		"bones"         : 0
+	},
+
+	"scale" : 1.000000,
+
+	"materials" : [	{
+		"DbgColor" : 15658734,
+		"DbgIndex" : 0,
+		"DbgName" : "Material.002",
+		"blending" : "NormalBlending",
+		"colorAmbient" : [0.0809526872776849, 0.09272754926573556, 0.6400000190734865],
+		"colorDiffuse" : [0.0809526872776849, 0.09272754926573556, 0.6400000190734865],
+		"colorSpecular" : [0.5, 0.5, 0.5],
+		"depthTest" : true,
+		"depthWrite" : true,
+		"shading" : "Lambert",
+		"specularCoef" : 50,
+		"transparency" : 1.0,
+		"transparent" : false,
+		"vertexColors" : false
+	},
+
+	{
+		"DbgColor" : 15597568,
+		"DbgIndex" : 1,
+		"DbgName" : "Material.001",
+		"blending" : "NormalBlending",
+		"colorAmbient" : [0.6400000190734865, 0.03937477230094033, 0.0],
+		"colorDiffuse" : [0.6400000190734865, 0.03937477230094033, 0.0],
+		"colorSpecular" : [0.5, 0.5, 0.5],
+		"depthTest" : true,
+		"depthWrite" : true,
+		"shading" : "Lambert",
+		"specularCoef" : 50,
+		"transparency" : 1.0,
+		"transparent" : false,
+		"vertexColors" : false
+	}],
+
+	"vertices" : [1.01731,1.01127,1,-0.982693,1.01127,1,1.01731,1.01127,-1,-0.982693,1.01127,-1,1,4.37114e-08,1,-1,4.37114e-08,1,1,-4.37114e-08,-1,-1,-4.37114e-08,-1],
+
+	"morphTargets" : [],
+
+	"normals" : [0,1,0],
+
+	"colors" : [],
+
+	"uvs" : [],
+
+	"faces" : [35,1,0,2,3,0,0,0,0,0,35,5,4,6,7,1,0,0,0,0],
+
+	"bones" : [],
+
+	"skinIndices" : [],
+
+	"skinWeights" : [],
+
+	"animation" : {}
+
+
+}
